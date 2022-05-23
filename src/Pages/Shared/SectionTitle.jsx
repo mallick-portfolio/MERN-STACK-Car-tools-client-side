c0e98391535b8a39934ae48fa204278f716c5438
mallick-portfolio <tamal@gmail.com>
2022-05-23 17:29:28 +0600

    added private route

--- src/Pages/Shared/SectionTitle.jsx
+++ src/Pages/Shared/SectionTitle.jsx
@@ -1,23 +1,23 @@
-import { faStarOfDavid } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faStarOfDavid } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const SectionTitle = ({title}) => {
+const SectionTitle = ({ title }) => {
   return (
     <div className="py-5">
-        <h1 class="text-center uppercase  text-3xl font-bold  leading-none tracking-normal">
-          {title}
-        </h1>
-        <div class="flex flex-row items-center justify-center py-">
-          <span class="h-1 w-24 bg-accent rounded-full mx-2"></span>
-          <FontAwesomeIcon
-            icon={faStarOfDavid}
-            class="h-12 fill-current text-accbg-accent "
-          />
-          <span class="h-1 w-24 bg-accent rounded-full mx-2"></span>
-        </div>
+      <h1 class="text-center uppercase  text-3xl font-bold  leading-none tracking-normal">
+        {title}
+      </h1>
+      <div class="flex flex-row items-center justify-center py-">
+        <span class="h-1 w-24 bg-accent rounded-full mx-2"></span>
+        <FontAwesomeIcon
+          icon={faStarOfDavid}
+          class="h-12 fill-current text-accbg-accent "
+        />
+        <span class="h-1 w-24 bg-accent rounded-full mx-2"></span>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default SectionTitle
+export default SectionTitle;
