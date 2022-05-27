6174f687050e8ecb3dcb97ff94aef652995a9e90
mallick-portfolio <tamal@gmail.com>
2022-05-27 15:51:49 +0600

    update firebase file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,24 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6SDR7u-rHNOUIlZsbxCC-QIqy8LR6w2Q",
-  authDomain: "garden-tools-8a9de.firebaseapp.com",
-  projectId: "garden-tools-8a9de",
-  storageBucket: "garden-tools-8a9de.appspot.com",
-  messagingSenderId: "729936257461",
-  appId: "1:729936257461:web:9a546e5c3e07579de3b9fc"
+  // apiKey: "AIzaSyB6SDR7u-rHNOUIlZsbxCC-QIqy8LR6w2Q",
+  // authDomain: "garden-tools-8a9de.firebaseapp.com",
+  // projectId: "garden-tools-8a9de",
+  // storageBucket: "garden-tools-8a9de.appspot.com",
+  // messagingSenderId: "729936257461",
+  // appId: "1:729936257461:web:9a546e5c3e07579de3b9fc"
+
+
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+
+
+
+
 };
 
 // Initialize Firebase
